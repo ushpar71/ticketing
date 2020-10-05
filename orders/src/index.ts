c79eaecc166ef96026032d67db146e54abbbaab7
Partha Ramaswami <partha@Partha-MacBook-Air.local>
2020-10-04 21:28:03 -0400

    workflow adddd

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
-  console.log('Orders Service Starting...');
+  console.log('Orders Service Starting......');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
