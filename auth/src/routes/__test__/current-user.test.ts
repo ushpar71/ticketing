209317f10a9018f6eb07a12fb1ea503a872b4f26
Partha Ramaswami <partha@Partha-MacBook-Air.local>
2020-10-04 20:42:44 -0400

    fixed test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -37,6 +37,6 @@ it('Responds with null if not authenticated', async () => {
   const response = await request(app)
     .get('/api/users/currentuser')
     .send()
-    .expect(400);
+    .expect(200);
   expect(response.body.currentUser).toEqual(null);
 });
